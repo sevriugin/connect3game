05054a412040ccf32c12cae589459b78e212a7ee
Sergei Sevriugin <sevriugin@gmail.com>
2019-12-25 14:40:54 +0300

    Initial Commit

---
+++ Connect 3 Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Connect 3 Game
+//
+//  Created by Sergey Sevriugin on 25/12/2019.
+//  Copyright © 2019 Sergey Sevriugin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
